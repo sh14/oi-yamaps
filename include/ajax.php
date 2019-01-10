a8a916e0c560093cd64717a42ea908162420f5ec
sh14 <sh-v@ya.ru>
2019-01-10 16:47:10 +0300

    refactor ajax and rest functions

--- include/ajax.php
+++ include/ajax.php
@@ -7,12 +7,15 @@
 namespace oiyamaps;
 
 function ajax_get_place() {
-	$result = array();
+
 	if ( ! empty( $_POST['place'] ) ) {
 		$result = get_place( $_POST['place'] );
+		if ( ! empty( $result ) ) {
+			wp_send_json_success( $result );
+		}
 	}
 
-	wp_send_json( $result );
+	wp_send_json_error();
 }
 
 add_action( 'wp_ajax_' . __NAMESPACE__ . '_' . 'ajax_get_place', __NAMESPACE__ . '\ajax_get_place' );
